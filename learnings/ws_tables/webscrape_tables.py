40732ce866a15a74d8331a4abf36f131f6d9c015
benicastro <benedict.zcastro@gmail.com>
2023-08-21 20:47:28 +0800

    Used pandas to output the scraped data as a csv file

--- learnings/ws_tables/webscrape_tables.py
+++ learnings/ws_tables/webscrape_tables.py
@@ -1,13 +1,37 @@
+# Import needed modules
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+# Set website url to scrape
 url = "https://www.skysports.com/premier-league-table"
-r = requests.get(url)
 
-soup = BeautifulSoup(r.text, "html.parser")
 
-league_table = soup.find("tbody")
+# Create function
+def scrape_tables(url_to_scrape):
+    """This function scrapes data scores from a table given a website url."""
 
-for row in league_table.find_all("tr"):
-    team = row.find_all("td")[1].text.strip()
-    print(team)
+    league_data = []
+
+    req = requests.get(url_to_scrape)
+    soup = BeautifulSoup(req.text, "html.parser")
+
+    league_table = soup.find("tbody")
+    for row in league_table.find_all("tr"):
+        team = row.find_all("td")[1].text.strip()
+        points = row.find_all("td")[9].text.strip()
+        team_data = {
+            "name": team,
+            "points": points,
+        }
+        league_data.append(team_data)
+
+    return league_data
+
+
+# Test function
+data = scrape_tables(url)
+
+# create data frame
+league_df = pd.DataFrame(data)
+league_df.to_csv("league_data.csv")
