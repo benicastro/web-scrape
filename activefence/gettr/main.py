c32eac8f98b0f1963d4e9ec3b82cc826507183d9
benicastro <benedict.zcastro@gmail.com>
2023-10-17 20:40:16 +0800

    Worked on scraping using httpx and selectolax

---
+++ activefence/gettr/main.py
@@ -0,0 +1,33 @@
+# 1. Check the website
+# 2. Understand the structure or how it feels
+# 3. Inspect elements
+
+import httpx
+from selectolax.parser import HTMLParser
+
+url = "https://www.rei.com/c/camping-and-hiking/f/scd-deals"
+# url = "https://gettr.com/user/valiantnews"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36",
+}
+
+response = httpx.get(url, headers=headers)
+html = HTMLParser(response.text)
+
+
+def extract_text(html, selector):
+    try:
+        return html.css_first(selector).text()
+    except AttributeError:
+        return None
+
+
+products = html.css("li.VcGDfKKy_dvNbxUqm29K")
+
+for product in products:
+    item = {
+        "name": extract_text(product, ".Xpx0MUGhB7jSm5UvK2EY"),
+        "price": extract_text(product, "span[data-ui=sale-price]"),
+    }
+
+    print(item)
