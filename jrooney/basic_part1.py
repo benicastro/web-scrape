08439375366d47164963e6c3d533a7e098712f24
benicastro <benedict.zcastro@gmail.com>
2023-10-24 00:52:36 +0800

    Wrote a code to scrape product info of a certain webpage using httpx and selectolax

---
+++ jrooney/basic_part1.py
@@ -0,0 +1,30 @@
+import httpx
+from selectolax.parser import HTMLParser
+
+url = "https://www.rei.com/c/camping-and-hiking/f/scd-deals"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36"
+}
+
+
+def extract_text(html, selector):
+    try:
+        return html.css_first(selector).text()
+    except AttributeError:
+        return None
+
+
+response = httpx.get(url, headers=headers)
+# print(response.status_code)
+# print(response.text) # HTML
+
+html = HTMLParser(response.text)
+# print(html.css_first("title").text())
+
+products = html.css("li.VcGDfKKy_dvNbxUqm29K")
+for product in products:
+    item = {
+        "name": extract_text(product, ".Xpx0MUGhB7jSm5UvK2EY"),
+        "price": extract_text(product, "span[data-ui=sale-price]"),
+    }
+    print(item)
